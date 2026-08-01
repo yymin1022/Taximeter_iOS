cece3748e5c41f669e73bad4835e5388d378ef31
yymin1022 <yymin1022@gmail.com>
2026-08-01 18:33:39 +0900

    Update MeterViewModel.swift : Added Meter ViewModel

---
+++ TaxiMeter/Presentation/Meter/MeterViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MeterViewModel.swift
+//  TaxiMeter
+//
+
+import Observation
+
+/// Meter ViewModel
+/// - Manages UI State for Meter View
+@Observable
+public final class MeterViewModel {
+    public init() {}
+}

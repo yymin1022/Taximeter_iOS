3c9dba084ba03aa248064e90856cf0f5d4ada2ce
yymin1022 <yymin1022@gmail.com>
2026-08-02 00:15:25 +0900

    Update HomeViewModel.swift : Added Home ViewModel

---
+++ TaxiMeter/Presentation/Main/Subscreen/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  TaxiMeter
+//
+
+import Observation
+
+/// Home ViewModel
+/// - Manages UI State for Home Subscreen
+@Observable
+public final class HomeViewModel {
+    public init() {}
+}

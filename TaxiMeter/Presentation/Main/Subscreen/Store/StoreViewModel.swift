e7b96f63b61cbbb6a5487f3f079e7dea34c834a9
yymin1022 <yymin1022@gmail.com>
2026-08-02 00:16:04 +0900

    Update StoreViewModel.swift : Added Store ViewModel

---
+++ TaxiMeter/Presentation/Main/Subscreen/Store/StoreViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StoreViewModel.swift
+//  TaxiMeter
+//
+
+import Observation
+
+/// Store ViewModel
+/// - Manages UI State for Store Subscreen
+@Observable
+public final class StoreViewModel {
+    public init() {}
+}

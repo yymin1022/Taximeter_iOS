59c33547ce431510988dc4ab71d825335c34851d
yymin1022 <yymin1022@gmail.com>
2026-08-01 18:33:33 +0900

    Update MainViewModel.swift : Added Main ViewModel

---
+++ TaxiMeter/Presentation/Main/MainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewModel.swift
+//  TaxiMeter
+//
+
+import Observation
+
+/// Main ViewModel
+/// - Manages UI State for Main View
+@Observable
+public final class MainViewModel {
+    public init() {}
+}

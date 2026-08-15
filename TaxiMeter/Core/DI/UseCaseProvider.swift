ff096f04af5bb396aa9fba4addaa84d62c3044a9
yymin1022 <yymin1022@gmail.com>
2026-08-15 14:28:44 +0900

    Update UseCaseProvider.swift : Added Meter UseCases to Provider

--- TaxiMeter/Core/DI/UseCaseProvider.swift
+++ TaxiMeter/Core/DI/UseCaseProvider.swift
@@ -6,12 +6,19 @@
 import Foundation
 
 /// Provider for UseCases
-public final class UseCaseProvider {
+public final class UseCaseProvider: @unchecked Sendable {
     public static let shared = UseCaseProvider()
 
     public let updateCostInfoUseCase: UpdateCostInfoUseCase
+    public let observeSpeedUseCase: ObserveSpeedUseCase
+    public let calculateMeterCostUseCase: CalculateMeterCostUseCase
 
     private init() {
         self.updateCostInfoUseCase = UpdateCostInfoUseCase()
+
+        let locationRepository = RepositoryProvider.shared.locationRepository
+        let observeSpeedUseCase = ObserveSpeedUseCase(locationRepository: locationRepository)
+        self.observeSpeedUseCase = observeSpeedUseCase
+        self.calculateMeterCostUseCase = CalculateMeterCostUseCase(observeSpeedUseCase: observeSpeedUseCase)
     }
 }
